5ab6ac87262b7597d0024077ff026c6cef08df5d
emsleyd <emsleyd@gmail.com>
2023-03-16 12:39:33 +0000

    starting on user registration

--- 3GoodThings/main_app/urls.py
+++ 3GoodThings/main_app/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+
+    #SITE
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('help/', views.help, name='help'),
     path('profile/', views.profile, name='profile'),
     path('stats/', views.stats, name='stats'),
-    path('days/', views.days_index, name='days'),
 
-
+    #USER
+    # path('register/', views.register_request, name='register'),
+    # path('signup/', SignUpView.as_view(), name='signup'),
 
 
 ]
--- 3GoodThings/main_app/views.py
+++ 3GoodThings/main_app/views.py
@@ -3,12 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-days = {
-    
-}
 
 def home(request):
-    return HttpResponse('<h1>3GT - 3 Good Things</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -21,7 +18,4 @@ def profile(request):
 
 def stats(request):
     return render(request, 'stats.html')
-
-def days_index(request):
-    return render(request, 'days/index.html', { 'days': days})
 
