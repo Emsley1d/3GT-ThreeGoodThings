b34f736b86c3e078540d3985670032cd529a2be5
emsleyd <emsleyd@gmail.com>
2023-03-16 11:30:15 +0000

    more basic files/folders created

--- 3GoodThings/main_app/urls.py
+++ 3GoodThings/main_app/urls.py
@@ -4,7 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('help/', views.help, name='help')
+    path('help/', views.help, name='help'),
+    path('profile/', views.profile, name='profile'),
+    path('stats/', views.stats, name='stats'),
+    path('days/', views.days_index, name='days'),
+
+
 
 
 ]
--- 3GoodThings/main_app/views.py
+++ 3GoodThings/main_app/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+days = {
+    
+}
+
 def home(request):
     return HttpResponse('<h1>3GT - 3 Good Things</h1>')
 
@@ -11,3 +15,13 @@ def about(request):
 
 def help(request):
     return render(request, 'help.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def stats(request):
+    return render(request, 'stats.html')
+
+def days_index(request):
+    return render(request, 'days/index.html', { 'days': days})
+
