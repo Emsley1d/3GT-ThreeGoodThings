0b77efad01bca3b48cc14be4652ee7b1910f2e86
emsleyd <emsleyd@gmail.com>
2023-05-01 10:51:17 +0100

    migration issue resolved, still working on email verification link

--- 3GoodThings/main_app/models.py
+++ 3GoodThings/main_app/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=100)
-    # email = forms.EmailField(max_length=200, help_text='Required') 
+    email = models.EmailField()
+    email_verified = models.BooleanField(default=False)
     email_verification_code = models.CharField(max_length=50, blank=True, null=True)
     password = models.CharField(max_length=128, default='')
-
+    last_login = models.DateTimeField(auto_now=True)
 
 
 class EmailVerification(models.Model):
     email = models.EmailField()
     verification_code = models.CharField(max_length=100)
     is_verified = models.BooleanField(default=False)
-    # email_verification_code = models.CharField(max_length=50, blank=True, null=True)
